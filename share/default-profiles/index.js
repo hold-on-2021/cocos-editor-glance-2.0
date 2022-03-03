f36862d9d1ac51586520f4e3a50ebdd07a976d5b
zehang <2210036910@qq.com>
2022-03-03 10:36:15 +0800

    add

--- share/default-profiles/index.js
+++ share/default-profiles/index.js
@@ -1,1 +1,2 @@
-"use strict";Editor.Profile.load("default://settings.json",require("./default-settings")),Editor.Profile.load("default://updates.json",require("./default-updates")),Editor.Profile.load("default://features.json",require("./default-features")),Editor.log("Load ~/.CocosCreator/settings.json"),Editor.App._profile=Editor.Profile.load("global://settings.json");
+"use strict";
+Editor.Profile.load("default://settings.json", require("./default-settings")), Editor.Profile.load("default://updates.json", require("./default-updates")), Editor.Profile.load("default://features.json", require("./default-features")), Editor.log("Load ~/.CocosCreator/settings.json"), Editor.App._profile = Editor.Profile.load("global://settings.json");
